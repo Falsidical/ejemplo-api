0a3778610e8be36f38f063643ecb3fe1d4b48d9f
Falsidical <franport@outlook.com>
2026-04-07 19:24:51 +0000

    Enable CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Creamos un servidor usando express
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const usersRouter = require('./routes/users');
 
 app.use(express.json());
+app.use(cors());
 app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
